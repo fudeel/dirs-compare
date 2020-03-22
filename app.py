84ab1bed4adfdd1980360c123613f914061e5a11
fudeel <fadilmucia@gmail.com>
2020-03-22 17:10:37 +0100

    get single folder info

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
-
+from tabulate import tabulate
 
 input_directory_1 = "D:\Fadil\Loops and Samples"
 input_directory_2 = "D:\Fadil\Music Production\Loops and Samples"
 
-inputs = [input_directory_1, input_directory_2]
+inputs = [input_directory_2]
+folders = []
+
+
+class FolderInfo:
+    def __init__(self, root, dirs_num, dirs):
+        self.root = root
+        self.dirs_num = dirs_num
+        self.dirs = dirs
+
+    def get_dirs(self):
+        return self.dirs
 
 
 def get_list():
@@ -18,7 +29,13 @@ def get_list():
             parent_dir = p.split(i, 1)[1].split("\\")[1]
             parent_dirs.append(parent_dir)
         parent_dirs = list(dict.fromkeys(parent_dirs))  # creating unique elements, since dicts accept only unique keys
-        print("The folder " + str(i) + "has: " + str(len(parent_dirs)) + " elements")
+
+        folder = FolderInfo(i, str(len(parent_dirs)), parent_dirs)
+        folders.append(folder)
 
 
 get_list()
+
+
+for f in folders:
+    print(f.get_dirs())
