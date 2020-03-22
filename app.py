a61514e780022bfc2376f04934e5fbf7deee7e26
fudeel <fadilmucia@gmail.com>
2020-03-22 17:58:51 +0100

    code clean

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import os
 input_directory_1 = "D:\Fadil\Loops and Samples"
 input_directory_2 = "D:\Fadil\Music Production\Loops and Samples"
 
-inputs = [input_directory_1, input_directory_2]
-folders = []
+folders = [input_directory_1, input_directory_2]
 
 
 def get_list():
     paths = []
     parent_dirs = []
-    for i in inputs:
-        for root, dirs, files in os.walk(i, topdown=False):
+    for current_folder in folders:
+        for root, dirs, files in os.walk(current_folder, topdown=False):
             paths.append(root)
         paths.pop()
         for p in paths:
-            parent_dir = p.split(i, 1)[1].split("\\")[1]
+            parent_dir = p.split(current_folder, 1)[1].split("\\")[1]
             parent_dirs.append(parent_dir)
         parent_dirs = list(dict.fromkeys(parent_dirs))  # creating unique elements, since dicts accept only unique keys
 
