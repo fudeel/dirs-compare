d303f261b558be73a34f8174909e42529f9bd8e0
fudeel <fadilmucia@gmail.com>
2020-03-22 15:52:49 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+folder_1 = []
+folder_2 = []
+
+input_directory_1 = "D:\Fadil\Loops and Samples"
+input_directory_2 = "D:\Fadil\Music Production\Loops and Samples"
+
+inputs = [input_directory_1]
+
+
+def get_list():
+    paths = []
+    parent_dirs = []
+    for i in inputs:
+        for root, dirs, files in os.walk(i, topdown=False):
+            paths.append(root)
+        paths.pop()
+        for p in paths:
+            parent_dir = p.split(i, 1)[1].split("\\")[1]
+            parent_dirs.append(parent_dir)
+        parent_dirs = list(dict.fromkeys(parent_dirs))  # creating unique elements, since dicts accept only unique keys
+        print("The folder " + str(i) + "has: " + str(len(parent_dirs)) + " elements")
+
+
+get_list()
